f7a64fc21c3d16ab0cb4e38940bfec82591b9ca1
Martin Lazarov <martin.lazarov@chaosgroup.com>
2016-04-10 23:04:54 +0300

    Upgrading google cloud monitoring API support to v3

--- index.js
+++ index.js
@@ -1,175 +1,182 @@
+'use strict';
+
 var google = require('googleapis');
-var cloudmonitoring = google.cloudmonitoring("v2beta2");
+var gmonitoring = google.monitoring("v3");
 var EventEmitter = require('events').EventEmitter;
 var util = require('util');
+
+var CUSTOM_METRIC_DOMAIN = 'custom.googleapis.com';
+
+function getNow() {
+    var d = new Date();
+    return JSON.parse(JSON.stringify(d).replace('Z', '000Z'));
+}
 
 var gcloudmon = function (options) {
     options = options || {};
 
-    this.project = options.project;
-    this.prefix = options.prefix || "custom.cloudmonitoring.googleapis.com";
+    this.prefix = options.prefix || CUSTOM_METRIC_DOMAIN;
+    this.project = 'projects/' + options.project;
 
-    this._authJSON = options.authJSON,
 
-    this._jwtClient = new google.auth.JWT(null,
-                                          null,
-                                          null,
-                                          "https://www.googleapis.com/auth/monitoring");
-    this._jwtClient.fromJSON(this._authJSON);
+    var monitoringScopes = [
+        'https://www.googleapis.com/auth/cloud-platform',
+        'https://www.googleapis.com/auth/monitoring',
+        'https://www.googleapis.com/auth/monitoring.read',
+        'https://www.googleapis.com/auth/monitoring.write'
+    ];
+
+    this.getMonitoringClient = function (callback) {
+        google.auth.fromJSON(options.authJSON, function (err, authClient){
+        if (err) {
+            return callback(err);
+        }
+        if (authClient.createScopedRequired && authClient.createScopedRequired()) {
+            authClient = authClient.createScoped(monitoringScopes);
+        }
+        callback(null, authClient);
+    });
+  }
 };
 
 util.inherits(gcloudmon, EventEmitter);
 
-gcloudmon.prototype.createMetric = function (name, labels, type, callback){
+gcloudmon.prototype.listMonitoredResourceDescriptors = function (callback){
     var self = this;
 
-    if(typeof(type) === 'function') {
-        callback = type;
-        type = undefined;
-    }
-
-    var point = {
-        "name": "custom.cloudmonitoring.googleapis.com/"+name,
-        "typeDescriptor": {
-            "metricType": "gauge",
-            "valueType": type ? type : "int64"
-        },
-        "labels": labels
-    }
-
-    var params = { auth: self._jwtClient,
-                   project: self.project,
-                   resource: point};
-
-    cloudmonitoring.metricDescriptors.create(params, function (err, data) {
-        if (err) {
-            err.name = name;
-            err.labels = labels;
-            self.emit("error", err);
-        }
-        if(typeof(callback) === 'function') callback(err,data);
-    });
-
-}
-
-gcloudmon.prototype.deleteMetric = function (name, callback){
-    var self = this;
-
-    var params = { auth: self._jwtClient,
-                   project: self.project,
-                   metric: encodeURIComponent(self._transformLabel(name))
-    };
-
-    cloudmonitoring.metricDescriptors.delete(params, function (err, data) {
-        if (err) {
-            err.metric = name;
-            self.emit("error", err);
-        }
-        if(typeof(callback) === 'function') callback(err, data);
-    });
-
-}
-
-gcloudmon.prototype.listMetrics = function (callback,searchstr){
-    var self = this;
-
-    var params = { auth: self._jwtClient,
-                   project: self.project,
-                   query: searchstr
-                 };
-
-    cloudmonitoring.metricDescriptors.list(params, function (err, data) {
-        if (err) {
-            self.emit("error", err);
-        }
-        callback(err,data);
+    self.getMonitoringClient(function (err, authClient) {
+        gmonitoring.projects.monitoredResourceDescriptors.list({
+            auth: authClient,
+            name: self.project,
+            pageSize: 5
+        }, callback);
     });
 }
 
-gcloudmon.prototype.setValue = function (name, value, labels, callback) {
+gcloudmon.prototype.listMetricDescriptors = function (callback){
     var self = this;
-    var point = {
-        "kind": "cloudmonitoring#writeTimeseriesRequest",
-        "timeseries": [
-            {
-                "timeseriesDesc": {
-                    "project": self.project,
-                    "metric": self.prefix + name,
-                    "labels": self._transformLables(labels)
-                },
-                "point": {
-                    "start": (new Date()).toISOString(),
-                    "end": (new Date()).toISOString(),
-                    [Number.isInteger(value) ? "int64Value" : "doubleValue"]: value
-                }
+
+    self.getMonitoringClient(function (err, authClient) {
+        gmonitoring.projects.metricDescriptors.list({
+            auth: authClient,
+            name: self.project,
+            pageSize: 5
+        }, callback);
+    });
+}
+
+gcloudmon.prototype.listGroups = function (callback){
+    var self = this;
+
+    self.getMonitoringClient(function (err, authClient) {
+        gmonitoring.projects.groups.list({
+            auth: authClient,
+            name: self.project,
+            pageSize: 10
+        }, callback);
+    });
+}
+
+gcloudmon.prototype.createMetric = function (params, callback){
+    var self = this;
+
+    self.getMonitoringClient(function (err, authClient) {
+        gmonitoring.projects.metricDescriptors.create({
+            auth: authClient,
+            name: self.project,
+            resource: {
+                name: params.name,
+                description: params.description,
+                displayName: params.displayName,
+                type: self.prefix + '/' + params.type,
+                labels: params.labels,
+                metricKind: params.metricKind || "GAUGE",
+                valueType: params.valueType || "INT64"
             }
-        ]
-    };
-
-    var params = { auth: self._jwtClient,
-                   project: self.project,
-                   resource: point
-                 };
-
-    cloudmonitoring.timeseries.write(params, function (err, data) {
-        if (err) {
-            err.value = value;
-            self.emit("error", err);
-        }
-        if(typeof(callback) === 'function') callback(err, data);
+          }, callback);
     });
-};
+}
 
-gcloudmon.prototype.setValues = function (values, callback) {
+gcloudmon.prototype.deleteMetric = function (metricType, callback){
     var self = this;
-    var point = {
-        "timeseries": values.map(function (v) {
-            return {
-                "timeseriesDesc": {
-                    "project": self.project,
-                    "metric": self.prefix + v.name,
-                    "labels": self._transformLables(v.labels)
-                },
-                "point": {
-                    "start": (new Date()).toISOString(),
-                    "end": (new Date()).toISOString(),
-                    [Number.isInteger(v.value) ? "int64Value" : "doubleValue"]: v.value
+
+    self.getMonitoringClient(function (err, authClient) {
+        gmonitoring.projects.metricDescriptors.delete({
+            auth: authClient,
+            name: self.project + '/metricDescriptors/' + self.prefix + '/' + metricType
+        }, callback);
+    });
+}
+
+gcloudmon.prototype.setValue = function(metricType, value, params, callback){
+    var self = this;
+    var now = getNow();
+
+    self.getMonitoringClient(function (err, authClient) {
+        gmonitoring.projects.timeSeries.create({
+            auth: authClient,
+            name: self.project,
+            resource: {
+                timeSeries: [{
+                    metric: {
+                        type: self.prefix + '/' + metricType,
+                        labels: params.labels
+                    },
+                    resource: {
+                        type: params.resourceType || "global"
+                    },
+                    metricKind: params.metricKind || "GAUGE",
+                    valueType: params.valueType || "INT64",
+                    points: {
+                        interval: {
+                            startTime: params.intervalStart || now,
+                            endTime: params.intervalEnd || now
+                        },
+                        value: {
+                            int64Value: value
+                        }
+                    }
+                }]
+           }
+        }, callback);
+    });
+}
+gcloudmon.prototype.setValues = function (data,callback){
+    var self = this;
+    var now = getNow();
+
+    self.getMonitoringClient(function (err, authClient) {
+
+        var resources = {
+            "timeSeries": data.map(function (params){
+                return {
+                    metric: {
+                        type: self.prefix + '/' + params.metricType,
+                        labels: params.labels
+                    },
+                    resource: {
+                        type: params.resourceType || "global"
+                    },
+                    metricKind: params.metricKind || "GAUGE",
+                    valueType: params.valueType || "INT64",
+                    points: {
+                        interval: {
+                            startTime: params.intervalStart || now,
+                            endTime: params.intervalEnd || now
+                        },
+                        value: {
+                            int64Value: params.metricValue
+                        }
+                    }
                 }
-            };
-        })
-    };
-
-    var params = { auth: self._jwtClient,
-                   project: self.project,
-                   resource: point};
-
-    cloudmonitoring.timeseries.write(params, function (err, data) {
-        if (err) {
-            err.values = values;
-            self.emit("error", err);
-        }
-        if(typeof(callback) === 'function') callback(err, data);
+            })
+        };
+        gmonitoring.projects.timeSeries.create({
+            auth: authClient,
+            name: self.project,
+            resource: resources
+        }, callback);
     });
-};
-
-gcloudmon.prototype._transformLables = function (labels) {
-    var self = this;
-    var l = {};
-
-    Object.keys(labels).map(function (label) {
-        full_label = self._transformLabel(label);
-        l[full_label] = labels[label];
-    });
-
-    return l;
-};
-
-gcloudmon.prototype._transformLabel = function(label){
-    self = this;
-    if (label.search(/\./) !== -1) { // use user defined prefix
-        return label;
-    }
-    return self.prefix + label;
 }
 
 module.exports = gcloudmon;
